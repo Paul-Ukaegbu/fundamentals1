65f202e4ce663bac5f05517b7d50b78b1cb03ecf
Paul.py <54999877+Paul-py07@users.noreply.github.com>
2020-04-08 21:14:00 -0500

    new

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace fundamentals1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i < 255; i++){
+                Console.Write(" " + i);
+            }
+
+            for(int i = 1; i<=100; i++){
+                  if(i%3 == 0){
+                      Console.WriteLine("fizz");
+                  }
+                  else if(i%5 == 0){
+                      Console.WriteLine("Buzz");
+                  }
+                  else{
+                      Console.WriteLine("fizzbuzz");
+                  }
+            }
+          
+
+
+        }
+    }
+}
